08437ebd961a1c26a0bd96257a4116f423c770e6
KurylevDM <kurylevdima31@gmail.com>
2024-11-08 13:29:11 +0300

    Add files via upload

---
+++ 1.1/Form3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Курылев
+{
+    public partial class Form3 : Курылев.Form1
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+    }
+}
