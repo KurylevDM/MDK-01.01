7b1425bb1030a8d747a92b155d8b6ff550ee757a
KurylevDM <kurylevdima31@gmail.com>
2024-11-15 14:55:23 +0300

    Add files via upload

---
+++ Лабороторная работа 2 Курылев/4.StatusStrip/ParentForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MdiApplication_Курылев_1._4
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+        public ParentForm()
+        {
+            InitializeComponent();
+            // Свойству Text панели spData устанавливается текущая дата
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ++openDocuments;
+                    ChildForm newChild = new ChildForm(); 
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " +
+                        ++openDocuments;
+            break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi
+                    (System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+        }
+
+        private void spWin_Click(object sender, EventArgs e)
+        {
+            spWin.Text = "Windows is cascade";
+        }
+
+        private void spData_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
